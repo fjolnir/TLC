c60a99bdf8886412bc75e3416977eb17e7acb8f6
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-03-30 16:56:34 +0900

    Added an example

---
+++ examples/window.lua
@@ -0,0 +1,41 @@
+-- Ported from LuaCocoa's MinimalAppKit example
+-- Creates a quit menu item and a window. Very minimal indeed.
+
+package.path = package.path .. ';../?.lua'
+local objc = require("objc")
+objc.loadFramework("AppKit")
+setmetatable(_G, {__index=objc})
+
+local NSApplicationActivationPolicyRegular = 0
+local NSTitledWindowMask = 1
+local NSBackingStoreBuffered = 2
+
+local NSApp = NSApplication:sharedApplication()
+NSApp:setActivationPolicy(NSApplicationActivationPolicyRegular)
+
+-- Create the menubar
+local menuBar = NSMenu:alloc():init()
+local appMenuItem = NSMenuItem:alloc():init()
+menuBar:addItem(appMenuItem)
+NSApp:setMainMenu(menuBar)
+
+-- Create the App menu
+local appMenu = NSMenu:alloc():init()
+local appName = NSProcessInfo:processInfo():processName()
+local quitTitle = "Quit " .. tostring(appName)
+quitMenuItem = NSMenuItem:alloc():initWithTitle_action_keyEquivalent(NSStr(quitTitle), SEL("terminate:"), NSStr("q"))
+appMenu:addItem(quitMenuItem)
+appMenuItem:setSubmenu(appMenu)
+
+-- Create a window
+local mainWindow = NSWindow:alloc():initWithContentRect_styleMask_backing_defer(CGRect(CGPoint(0, 0), CGSize(200, 200)), NSTitledWindowMask, NSBackingStoreBuffered, false)
+mainWindow:cascadeTopLeftFromPoint(CGPoint(20,20))
+mainWindow:setTitle(appName)
+mainWindow:makeKeyAndOrderFront(NSApp)
+
+-- Bring the app out
+NSApp:activateIgnoringOtherApps(true)
+NSApp:run()
+
+
+
